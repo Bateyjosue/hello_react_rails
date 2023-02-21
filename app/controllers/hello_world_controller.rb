767ee319dc57596d73297f55f131249afccd84ac
Bateyjosue <josuebatey19@gmail.com>
2023-02-21 17:12:53 +0200

    Fix Rubocop

--- app/controllers/hello_world_controller.rb
+++ app/controllers/hello_world_controller.rb
@@ -1,10 +1,8 @@
-# frozen_string_literal: true
-
 class HelloWorldController < ApplicationController
   # layout "hello_world"
 
   def index
     @greet = Greeting.order('RANDOM()').first
-    render :json => @greet.text
+    render json: @greet.text
   end
 end
