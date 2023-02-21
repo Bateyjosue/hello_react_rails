6310e2dd05f5d28dfb76b7942f7ef7a375555119
Bateyjosue <josuebatey19@gmail.com>
2023-02-21 15:11:34 +0200

    Add data to database and update the seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Greeting.create([
+  {text: 'Hey, Heya'},
+  {text: 'Howdy'},
+  {text: 'It’s a pleasure to meet you'},
+  {text: 'Good morning'},
+])
