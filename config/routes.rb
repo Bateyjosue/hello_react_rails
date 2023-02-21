5bd7918292ddf47ef002f31d88cec8d465daf48b
Bateyjosue <josuebatey19@gmail.com>
2023-02-21 06:31:24 +0200

    COnfigure webpack

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'hello_world', to: 'hello_world#index'
+  root "hello_world#index"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
