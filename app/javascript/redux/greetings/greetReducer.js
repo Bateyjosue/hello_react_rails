8e7eddf15e70b4c9d1a04cf317cf71700e482f64
Bateyjosue <josuebatey19@gmail.com>
2023-02-21 15:09:57 +0200

    Add reducers and action to fetch the random greetings

--- app/javascript/redux/greetings/greetReducer.js
+++ app/javascript/redux/greetings/greetReducer.js
@@ -1,27 +1,23 @@
 import axios from 'axios'
 import {createAsyncThunk} from '@reduxjs/toolkit';
 
-const BASE_URL = 'https://127.0.0.1:3000';
+const BASE_URL = 'http://127.0.0.1:3000/';
 
-const reducerGreet = (state = '', action) => {
+export const getGreeting = createAsyncThunk(
+  'GET_GREETING',
+  async() => {
+      const response = await axios.get(BASE_URL);
+      console.log(response.data)
+      return response.data
+    }
+);
+
+const reducerGreet = (state = 'Greetings', action) => {
   switch (action.type) {
     case 'GET_GREETING/fulfilled': return action.payload
     default: return state
   }
 }
 
-export const getGreeting = createAsyncThunk(
-  'GET_GREETING',
-  async() => {
-    try {
-      const response = await axios.get(BASE_URL);
-      return response.data.greet
-    }
-    catch (err) {
-      console.log(err);
-    }
-
-    }
-)
 
 export default reducerGreet;
